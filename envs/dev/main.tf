e064519ba1b116f0d76f08a4ed1dbf1ba237f110
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2025-03-31 15:53:39 -0300

    feat(tf): add backend.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -8,8 +8,6 @@ module "vpc" {
   cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
    enable_dns_hostnames = true
-  tags = {
-    Environment = "dev"
-    Owner       = "Giovanni"
-  }
+  tags = local.common_tags 
+  
 }
