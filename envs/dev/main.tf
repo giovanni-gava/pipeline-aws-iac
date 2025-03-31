7f0294b355a62a45a5f1eac06b33dd5dfbc32b9c
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2025-03-31 12:36:31 -0300

    feat(tf): add backend.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "vpc" {
-  source          = "../../modules/vpc"
-  name            = "dev-vpc"
-  cidr_block      = "10.1.0.0/16"
+  source               = "../../modules/vpc"
+  name                 = "dev-vpc"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
