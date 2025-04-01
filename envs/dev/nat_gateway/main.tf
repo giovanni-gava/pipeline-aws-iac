eca04e0fd6e129a51301fd2ad43239e930cc14e9
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2025-04-01 11:19:10 -0300

    feat(tf): add public and private subnets

--- envs/dev/nat_gateway/main.tf
+++ envs/dev/nat_gateway/main.tf
@@ -14,10 +14,10 @@ module "nat_gateway" {
 
   nat_gateways = {
     "eu-west-1a" = {
-      public_subnet_id = data.terraform_remote_state.subnet.outputs.public_subnet_ids_by_az["eu-west-1a"][0]
+      public_subnet_id = data.terraform_remote_state.private_subnet.outputs.public_subnet_ids_by_az["eu-west-1a"][0]
     },
     "eu-west-1b" = {
-      public_subnet_id = data.terraform_remote_state.subnet.outputs.public_subnet_ids_by_az["eu-west-1b"][0]
+      public_subnet_id = data.terraform_remote_state.private_subnet.outputs.public_subnet_ids_by_az["eu-west-1b"][0]
     }
   }
 }
